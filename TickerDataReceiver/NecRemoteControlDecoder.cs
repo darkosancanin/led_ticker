8b8c506cbe18272052def35bff5fa4186c18b9a9
Darko Sancanin <darko.sancanin@gmail.com>
2011-02-18 20:25:47 +1100

    Added ticker data receiver netduino project

---
+++ TickerDataReceiver/NecRemoteControlDecoder.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Threading;
+using Microsoft.SPOT;
+using Microsoft.SPOT.Hardware;
+using SecretLabs.NETMF.Hardware;
+using SecretLabs.NETMF.Hardware.NetduinoPlus;
+
+namespace Netduino.TickerDataReceiver
+{
+    public class NecRemoteControlDecoder
+    {
+        private Cpu.Pin _irReceiverPin;
+        private long[] pulses;
+        private int currentPulseIndex;
+        private Timer irPulseTimeOutTimer;
+        public delegate void IrCommandReceivedEventHandler(UInt32 irData);
+        public event IrCommandReceivedEventHandler OnIrCommandReceived;
+        private InterruptPort irReceiverPort;
+
+        public NecRemoteControlDecoder(Cpu.Pin irReceiverPin)
+        {
+            _irReceiverPin = irReceiverPin;
+            pulses = new long[200];
+            currentPulseIndex = 0;
+            irReceiverPort = new InterruptPort(irReceiverPin, false, Port.ResistorMode.Disabled, Port.InterruptMode.InterruptEdgeBoth);
+            irReceiverPort.OnInterrupt += new NativeEventHandler(irReceiverPort_OnInterrupt);
+            irPulseTimeOutTimer = new Timer(new TimerCallback(IrPulseTimeOut), null, Timeout.Infinite, Timeout.Infinite);
+        }
+
+        private void irReceiverPort_OnInterrupt(uint data1, uint data2, DateTime time)
+        {
+            if (currentPulseIndex >= 200) currentPulseIndex = 0;
+            pulses[currentPulseIndex] = time.Ticks;
+            currentPulseIndex++;
+            irPulseTimeOutTimer.Change(10, Timeout.Infinite);
+        }
+
+        private void ConvertPulsesToMicroseconds(long numberOfPulses)
+        {
+            var firstMicrosecondsValue = pulses[0] / (TimeSpan.TicksPerMillisecond / 1000);
+            var lastMicosecondsValue = firstMicrosecondsValue;
+            for (int i = 1; i < numberOfPulses; i++)
+            {
+                var currentPulseMicrosecondsValue = pulses[i] / (TimeSpan.TicksPerMillisecond / 1000);
+                var currentPulseLength = currentPulseMicrosecondsValue - lastMicosecondsValue;
+                lastMicosecondsValue = currentPulseMicrosecondsValue;
+                pulses[i - 1] = currentPulseLength;
+            }
+        }
+
+        private void IrPulseTimeOut(object state)
+        {
+            var numberOfPulses = currentPulseIndex;
+            currentPulseIndex = 0;
+            irPulseTimeOutTimer.Change(Timeout.Infinite, Timeout.Infinite);
+
+            ConvertPulsesToMicroseconds(numberOfPulses);
+            if (numberOfPulses != 68) return;
+            if (!IsPulseMatch(pulses[0], 9000) || !IsPulseMatch(pulses[1], 4500)) return;
+
+            UInt32 data = 0;
+            for(int i = 65; i >= 3; i--){
+                if((i % 2) == 1){
+                    data = data << 1;
+                    if (IsPulseMatch(pulses[i], 565))
+                        data = data | 1;
+                }
+                else{
+                    if(!IsPulseMatch(pulses[i], 560))
+                        return;
+                    }
+            }
+
+            if (OnIrCommandReceived != null)
+                OnIrCommandReceived(data);
+        }
+
+        private bool IsPulseMatch(long actualValue, long expectedValue)
+        {
+            var marginOfError = 200;
+            if ((expectedValue - marginOfError) < actualValue && (expectedValue + marginOfError) > actualValue)
+                return true;
+            return false;
+        }
+    }
+}
---
+++ TickerDataReceiver/Program.cs
@@ -0,0 +1,111 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Threading;
+using System.Text;
+using Microsoft.SPOT;
+using Microsoft.SPOT.Hardware;
+using SecretLabs.NETMF.Hardware;
+using SecretLabs.NETMF.Hardware.NetduinoPlus;
+using Socket = System.Net.Sockets.Socket;
+using Microsoft.SPOT.Net.NetworkInformation;
+using System.IO.Ports;
+
+namespace Netduino.TickerDataReceiver
+{
+    public class Program
+    {
+        private static SerialPort _serialPort;
+
+        public static void Main()
+        {
+            var networkInterfaces = NetworkInterface.GetAllNetworkInterfaces();
+            Debug.Assert(networkInterfaces[0] != null);
+            var net = networkInterfaces[0];
+
+            Debug.Print("DHCP Enabled: " + net.IsDhcpEnabled);
+            Debug.Print("IP Address: " + net.IPAddress);
+
+            var necRemoteControlDecoder = new NecRemoteControlDecoder(Pins.GPIO_PIN_D7);
+            necRemoteControlDecoder.OnIrCommandReceived += necRemoteControlDecoder_OnIrCommandReceived;
+
+            _serialPort = new SerialPort(SerialPorts.COM1, 9600, Parity.None, 8, StopBits.One);
+            _serialPort.Open();
+
+            Thread.Sleep(Timeout.Infinite);
+        }
+
+        static void necRemoteControlDecoder_OnIrCommandReceived(UInt32 irData)
+        {
+            Debug.Print("Ir Command Received: " + irData);
+            var url = RemoteControlCodes.GetUrlFromCode(irData);
+            var tickerData = GetDataFromWeb(url);
+            Debug.Print(tickerData);
+            System.Text.UTF8Encoding encoder = new System.Text.UTF8Encoding();
+            var bytesToSend = encoder.GetBytes(tickerData);
+            _serialPort.Write(bytesToSend, 0, bytesToSend.Length);
+        }
+
+        private static string GetDataFromWeb(String url)
+        {
+            var httpRequest = HttpWebRequest.Create(url);
+            WebResponse webResponse = null;
+            try
+            {
+                webResponse = httpRequest.GetResponse();
+            }
+            catch (Exception e)
+            {
+                Debug.Print("Exception in HttpWebRequest.GetResponse(): " + e.ToString());
+            }
+            
+            var responseStream = webResponse.GetResponseStream();
+            var byteData = new byte[500];
+            var charData = new char[500];
+            var bytesRead = responseStream.Read(byteData, 0, byteData.Length);
+            int byteUsed, charUsed;
+            bool completed = false;
+            System.Text.Encoding.UTF8.GetDecoder().Convert(byteData, 0, bytesRead, charData, 0, bytesRead, true, out byteUsed, out charUsed, out completed);
+            return new String(charData, 0, charUsed);
+        }
+
+        public static class RemoteControlCodes
+        {
+            public static long Zero = 217268479;
+            public static long One = 284115199;
+            public static long Two = 300826879;
+            public static long Three = 317538559;
+            public static long Four = 350961919;
+            public static long Five = 367673599;
+            public static long Six = 384385279;
+            public static long Seven = 417808639;
+            public static long Eight = 434520319;
+            public static long Nine = 451231999;
+            public static long Power = 16728319;
+            public static long FunctionStop = 50151679;
+            public static long Up = 183845119;
+
+            public static string GetUrlFromCode(UInt32 irData)
+            {
+                var baseUrl = "http://darkosancanin.com/tickerdata/?option=";
+
+                if (irData == Zero)
+                    return baseUrl + 0;
+                if (irData == One)
+                    return baseUrl + 1;
+                if (irData == Two)
+                    return baseUrl + 2;
+                if (irData == Three)
+                    return baseUrl + 3;
+                if (irData == Four)
+                    return baseUrl + 4;
+                if (irData == Five)
+                    return baseUrl + 5;
+                if (irData == Six)
+                    return baseUrl + 6;
+                
+                return baseUrl + 0;
+            }
+        }
+    }
+}
